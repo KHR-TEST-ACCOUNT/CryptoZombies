91c30a5bb8e464ea63ff6d8f4fadff0ae155f0bf
koya <azumabass@gmail.com>
2023-04-07 11:59:28 +0900

    スマートコントラクトのデプロイを実装

--- migrations/2_crypto_zombies.js
+++ migrations/2_crypto_zombies.js
@@ -1,16 +1,21 @@
 /** @format */
 
 /**
+ * Truffle compile
+ * コントラクトのコードをコンパイルして、ビルドフォルダに保存する。
+ * ビルドフォルダには、書き換えられたコードと、ABIが保存される。
+ *
  * Migrations.sol → 変更の履歴を保存している
  *  → ２回目のデプロイをしなくて良いようにする。
  * デプロイ→ 1,2,3 の順で実行される。
  * マイグレーションをして、コントラクトの状態をどのように変更するかを
  * Truffleに伝える。  → コントラクトごとにMigration ファイルを分ける。
+ * おそらく、アーティファクトで宣言したコントラクトのABIを取得している。
  *
  * メインネット → truffle migrate --network mainnet
  */
-const Migrations = artifacts.require('Migrations');
+const CryptoZombies = artifacts.require('./CryptoZombies.sol');
 
 module.exports = function (deployer) {
-    deployer.deploy(Migrations);
+    deployer.deploy(CryptoZombies);
 };
