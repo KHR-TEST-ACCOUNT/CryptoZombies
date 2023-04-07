e8a3334be98b155c55ddfb6d5728889f7df46b0a
koya <azumabass@gmail.com>
2023-04-07 20:57:46 +0900

    移管プログラムのシナリオ１テストの実装

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -15,6 +15,7 @@ let cryptoZombies;
 //test code → CryptoZombies は テストするコントラクト（ビルド）
 contract('CryptoZombies', (accounts) => {
     // Ganash のアカウントを格納する  alice→０, bob→１
+    // accountsアドレスがそれぞれに格納されている。
     let [alice, bob] = accounts;
 
     // Hooks → ITの前に、インスタンスを生成する
@@ -32,7 +33,9 @@ contract('CryptoZombies', (accounts) => {
             from: alice,
         });
         /**
+         * Result -> Truffleがスマートコントラクトによって生成されたイベントログを自動的に提供
          * result.logs[0].args.nameのように、アリスの新しく作ったゾンビの名前を取得できる
+         * idと_dna も取得できる
          * result.tx：トランザクションハッシュ
          * result.receipt: トランザクションのレシートを含むオブジェクト。
          * result.receipt.status が true の場合、トランザクションが成功したことを意味する
@@ -49,5 +52,39 @@ contract('CryptoZombies', (accounts) => {
                 from: alice,
             })
         );
+    });
+
+    /**
+     * グループ化する → Contextで分ける。
+     *  → グループを、シナリオという。 → 保守性が高くなるので、グルーピングする。
+     *  → シナリオ１，２ に分けてテストすることで、他のコーダーに伝える事ができる。
+     * Transfer 関数（移動）が、２種類のメソッドを提供しているのでそれをテストする。
+     *
+     * x を前につけることによってテストをスキップできる。 → xcontext
+     */
+    context('1ステップ目の転送シナリオをテスト', async () => {
+        it('ゾンビをAからBに転送させる', async () => {
+            // ゾンビを生成
+            const result = await cryptoZombies.createRandomZombie(
+                zombieNames[0],
+                {
+                    from: alice,
+                }
+            );
+            // 生成したゾンビのIDを数値に変換して格納 → ブロックを参照しないので非同期じゃない。
+            const zombieId = result.logs[0].args.zombieId;
+            // AliceのアドレスとゾンビのID を、アリスの名前で関数に渡す
+            await cryptoZombies.transfer(bob, zombieId, { from: alice });
+            // ゾンビの新しいオーナーを取得（移管を確認）
+            const newOwner = await cryptoZombies.ownerOf(zombieId);
+            // Bob のアドレスが新しいオーナーと等しいかどうかをアサート
+            assert.equal(bob, newOwner);
+        });
+    });
+
+    xcontext('2ステップ目の転送シナリオをテスト', async () => {
+        it('承認されたアドレスがtransferFromを呼び出したら、ゾンビを承認して転送を許可する。', async () => {});
+
+        it('オーナーがtransferFromを呼び出したら、承認してゾンビを転送する。', async () => {});
     });
 });
